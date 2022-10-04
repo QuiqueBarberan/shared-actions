123fd27ed8806ec3712453957e0076cb2eb3f134
QuiqueBarberan <113335738+QuiqueBarberan@users.noreply.github.com>
2022-10-04 17:26:06 +0200

    add console.log

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const exec = require("@actions/exec");
 
 async function run() {
     try {
+        console.log('1.-Principio');
+        console.log('2.-core.getInput('version-type')');
         const versionType = core.getInput('version-type');
+        
         const src = __dirname;
-
+        console.log('3.-git_update con variable versionType');
+        console.log(${versionType});
         await exec.exec(`${src}/git_update.sh -v ${versionType}`);
     } catch (error) {
         core.setFailed(error.message);
