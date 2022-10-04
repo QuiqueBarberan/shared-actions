3234f78152fbbc8f048749629313e0d4f60a90c5
Quique Barberan <ebarberan@hiberus.com>
2022-10-04 17:29:43 +0200

    put some console.log

--- action/index.js
+++ action/index.js
@@ -4113,9 +4113,13 @@ const exec = __nccwpck_require__(49);
 
 async function run() {
     try {
+        console.log('1.-Principio');
+        console.log('2.-core.getInput(version-type)');
         const versionType = core.getInput('version-type');
+        
         const src = __dirname;
-
+        console.log('3.-git_update con variable versionType');
+        console.log(versionType);
         await exec.exec(`${src}/git_update.sh -v ${versionType}`);
     } catch (error) {
         core.setFailed(error.message);
